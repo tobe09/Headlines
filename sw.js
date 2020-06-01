127cb8492fcb08caf39485b75417b48ca2e0fac9
Tobenna <tobenna@nexus23rdltd.com>
2020-06-01 10:57:45 +0100

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿self.importScripts('/js/idb.js');                //import indexed db promise file
+﻿self.importScripts('js/idb.js');                //import indexed db promise file
 
 const staticCache = 'headlines-static-3'; 
 const imgCache = 'headlines-content-imgs';
